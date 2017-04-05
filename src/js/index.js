8852a5830da3cd725e14ba5bd40c1f7740efb414
mario.vazquez <marioal.vazquez@gmail.com>
2017-04-05 16:53:34 -0500

    First Version of my Weather Web App

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  $(".date").text(new Date().toUTCString())
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position){
+      var crds = position.coords;
+      var lat = crds.latitude;
+      var lon = crds.longitude;
+      console.log("lat: " + lat + "\n lon: " + lon);
+      $.ajax({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyCraklwPHfcXSXbAobA4j5wjC-UGlWVaiU`
+      }).done(function(data){
+        console.log(data);
+        $(".city").text(data.results[0]['formatted_address'])
+      }).fail(function(data){
+        console.log(data);
+      });
+
+      //Weather
+      $.ajax({
+        url: `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=28201f8518be8384a0fbf65ed0d28a68`
+      }).done(function(data){
+        console.log(data);
+        var temp = data.main.temp;
+        temp = temp - 273.15;
+        $(".temp").text(`${temp} °C`);
+        $(".temp-text").text(data.weather[0]['description']);
+        console.log(temp);
+      }).fail(function(data){
+        console.log("Weather", data);
+      })
+    });
+  }
+  else{
+    alert("Enable geolocation services to continue");
+  }
+});
